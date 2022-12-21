f6705bff7c3b6c8096bf286fda132d65f7a1c577
mdwitr0 <mdwit0r@gmail.com>
2022-12-22 00:05:57 +0100

    feat(bot): add bot scenes script

--- src/constants/buttons.const.ts
+++ src/constants/buttons.const.ts
@@ -1,6 +1,10 @@
 const BUTTONS = {
   GET_ACCESS: 'Я хочу получить доступ к API 🚀',
   QUESTION: 'У меня есть вопрос 🥹',
-  FREE_TARIFF: 'Бесплатный тариф 🔥',
-  MORE_TARIFF: 'Мне нужно больше 🔥🔥🔥',
+  FREE_TARIFF: '200 🔥',
+  DEVELOPER_TARIFF: '5000 🔥🔥',
+  UNLIMITED_TARIFF: 'Безлимит 🔥🔥🔥',
+  JOIN_CHAT: 'Вступить в чат',
+  CONFIRM_JOIN_CHAT: 'Я вступил в чат',
+  GET_TOKEN: 'Получить токен',
 };
--- src/constants/scenes.const.ts
+++ src/constants/scenes.const.ts
@@ -1,3 +1,58 @@
-const COMMANDS = {
+import { CommandEnum } from '../enum/command.enum';
 
-}
+const SCENES = {
+  [CommandEnum.START]: {
+    text: 'Привет! Я бот который поможет тебе получить токен для работы с API kinopoisk.dev. Для начала выбери действие:',
+    buttons: [BUTTONS[CommandEnum.GET_ACCESS], BUTTONS[CommandEnum.QUESTION]],
+  },
+  [CommandEnum.GET_ACCESS]: {
+    text: `Для получения доступа к API тебе нужно выбрать тарифный план. \n\nТарифы: \n${
+      BUTTONS[CommandEnum.FREE_TARIFF]
+    } - Всегда бесплатный.\n${
+      BUTTONS[CommandEnum.DEVELOPER_TARIFF]
+    } - 500 рублей в месяц.\n${
+      BUTTONS[CommandEnum.UNLIMITED_TARIFF]
+    } - 2000 рублей в месяц.`,
+    buttons: [
+      BUTTONS[CommandEnum.FREE_TARIFF],
+      BUTTONS[CommandEnum.DEVELOPER_TARIFF],
+      BUTTONS[CommandEnum.UNLIMITED_TARIFF],
+    ],
+    scenes: {
+      [CommandEnum.FREE_TARIFF]: {
+        text: `Отлично! Но перед этим к тебе есть небольшая просьба, зайди к нам в общий чат 😇\n В нем ты всегда можешь получить поддержку от сообщества и администрации, а в замен я дам тебе токен!`,
+        buttons: [BUTTONS[CommandEnum.JOIN_CHAT]],
+        scenes: {
+          [BUTTONS[CommandEnum.JOIN_CHAT]]: {
+            text: `Отлично, возвращайся сюда после вступления в чат!`,
+            buttons: [BUTTONS[CommandEnum.CONFIRM_JOIN_CHAT]],
+          },
+          [BUTTONS[CommandEnum.CONFIRM_JOIN_CHAT]]: {
+            success: {
+              text: `Отлично! Теперь ты можешь получить токен!`,
+              buttons: [BUTTONS[CommandEnum.GET_TOKEN]],
+            },
+          },
+        },
+      },
+      [CommandEnum.DEVELOPER_TARIFF]: {
+        text: `Оплата пока что не доступна, свяжитесь с администрацией для получения доступа. \n\n Главный разработчик: @mdwit`,
+      },
+      [CommandEnum.UNLIMITED_TARIFF]: {
+        text: `Оплата пока что не доступна, свяжитесь с администрацией для получения доступа. \n\n Главный разработчик: @mdwit`,
+      },
+    },
+  },
+  [CommandEnum.QUESTION]: {
+    text: `Если у тебя есть вопрос, то ты можешь задать его в нашем чате. \n\n Чтобы вступить в чат нажми на кнопку ниже.`,
+    buttons: [BUTTONS[CommandEnum.JOIN_CHAT]],
+  },
+  [CommandEnum.GET_REQUEST_STATS]: {
+    success: {
+      text: `Вот статистика по использованию API:`,
+    },
+    error: {
+      text: `Вы еще не зарегистрированы в системе, для этого вам нужно получить токен.`,
+    },
+  },
+};
