f278f7c2a6288c4dfa071aa82e266cf625175f4e
mdwitr0 <mdwit0r@gmail.com>
2022-12-22 03:05:41 +0100

    chore(bot): rename state interface

--- src/interfaces/context.interface.ts
+++ src/interfaces/context.interface.ts
@@ -4,15 +4,15 @@ import { Update } from 'telegraf/typings/core/types/typegram';
 export interface Context extends BaseContext {
   update: Update.CallbackQueryUpdate;
   session: SessionData;
-  scene: Scenes.SceneContextScene<Context, MySceneSession>;
+  scene: Scenes.SceneContextScene<Context, SceneSession>;
   match: any;
 }
 
-interface SessionData extends Scenes.SceneSession<MySceneSession> {
+interface SessionData extends Scenes.SceneSession<SceneSession> {
   messageId: number;
 }
 
-interface MySceneSession extends Scenes.SceneSessionData {
+interface SceneSession extends Scenes.SceneSessionData {
   state: {
     token?: string;
   };
