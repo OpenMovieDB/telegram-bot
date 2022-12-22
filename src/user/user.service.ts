dee0091bd8cf7db3a66baf1352d7412a34d7f000
mdwitr0 <mdwit0r@gmail.com>
2022-12-23 00:39:02 +0100

    feat(user): remove duplicate schemas

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { Model } from 'mongoose';
 import { InjectModel } from '@nestjs/mongoose';
-import { User, UserDocument } from '../schemas/user.schema';
+import { User, UserDocument } from './schemas/user.schema';
 import ApiKey from 'uuid-apikey';
 
 @Injectable()
