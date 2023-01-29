5ecfeae579216c861c016bcd3be2c2a7c12576ae
mdwitr0 <mdwit0r@gmail.com>
2023-01-29 10:52:22 +0100

    feat(billing): add status & system

---
+++ src/billing/enum/payment-status.enum.ts
@@ -0,0 +1,6 @@
+export enum PaymentStatusEnum {
+  PENDING = 'PENDING',
+  SUCCESS = 'SUCCESS',
+  FAILED = 'FAILED',
+  CANCELED = 'CANCELED',
+}
---
+++ src/billing/enum/payment-system.enum.ts
@@ -0,0 +1,3 @@
+export enum PaymentSystemEnum {
+  CYPTOMUS = 'CYPTOMUS',
+}
