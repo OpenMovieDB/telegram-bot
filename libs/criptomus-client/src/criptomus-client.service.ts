20337108ba216c66165238041e3b8e1abeb5e594
mdwitr0 <mdwit0r@gmail.com>
2023-02-01 20:42:00 +0100

    chore(criptomus): add sign generation

--- libs/criptomus-client/src/criptomus-client.service.ts
+++ libs/criptomus-client/src/criptomus-client.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { HttpService } from '@nestjs/axios';
 import { ConfigService } from '@nestjs/config';
+import * as crypto from 'crypto';
 
 @Injectable()
 export class CriptomusClientService {
@@ -13,5 +14,18 @@ export class CriptomusClientService {
   ) {
     this.apiKey = this.configService.get('CRIPTOMUS_API_KEY');
     this.merchantId = this.configService.get('CRIPTOMUS_MERCHANT_ID');
+  }
+
+  getHeaders(payload: { [key: string]: any }) {
+    const payloadString = JSON.stringify(payload);
+    const sign = crypto
+      .createHash('md5')
+      .update(Buffer.from(payloadString) + this.apiKey)
+      .digest('hex');
+
+    return {
+      merchant: this.merchantId,
+      sign,
+    };
   }
 }
