509125dc03c074d0a6c9468af57a39c76cfe46c7
mdwitr0 <mdwit0r@gmail.com>
2023-02-01 20:34:33 +0100

    chore(criptomus): add credentials

--- libs/criptomus-client/src/criptomus-client.service.ts
+++ libs/criptomus-client/src/criptomus-client.service.ts
@@ -1,4 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { HttpService } from '@nestjs/axios';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
-export class CriptomusClientService {}
+export class CriptomusClientService {
+  private readonly apiKey: string;
+  private readonly merchantId: string;
+
+  constructor(
+    private readonly httpService: HttpService,
+    private readonly configService: ConfigService,
+  ) {
+    this.apiKey = this.configService.get('CRIPTOMUS_API_KEY');
+    this.merchantId = this.configService.get('CRIPTOMUS_MERCHANT_ID');
+  }
+}
