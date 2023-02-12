f139d527b1d6bcd4760d474522fa36e0a489804c
Evgenii Fedoryachenko <mdwit0r@gmail.com>
2023-02-13 00:11:10 +0100

    feat(billing): add create payment & validate payment methods

--- src/billing/billing.module.ts
+++ src/billing/billing.module.ts
@@ -1,7 +1,24 @@
-import { Module } from '@nestjs/common';
+import { CriptomusClient, CriptomusClientModule } from '@app/criptomus-client';
+import { Payment, PaymentSchema } from './schemas/payment.schema';
+import { Tariff, TariffSchema } from 'src/tariff/schemas/tariff.schema';
+import { User, UserSchema } from 'src/user/schemas/user.schema';
+
 import { BillingService } from './billing.service';
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { PaymentStrategyFactory } from './strategies/factory/payment-strategy.factory';
+import { TariffModule } from 'src/tariff/tariff.module';
+import { UserModule } from 'src/user/user.module';
+import { UserService } from 'src/user/user.service';
 
 @Module({
-  providers: [BillingService],
+  imports: [
+    MongooseModule.forFeature([{ name: Payment.name, schema: PaymentSchema }]),
+    UserModule,
+    TariffModule,
+    CriptomusClientModule,
+  ],
+  providers: [BillingService, PaymentStrategyFactory],
+  exports: [BillingService],
 })
 export class BillingModule {}
--- src/billing/billing.service.ts
+++ src/billing/billing.service.ts
@@ -1,41 +1,64 @@
-import { Injectable } from '@nestjs/common';
-import { InjectModel } from '@nestjs/mongoose';
-import { Model } from 'mongoose';
 import { Payment, PaymentDocument } from './schemas/payment.schema';
+
+import { InjectModel } from '@nestjs/mongoose';
+import { Injectable } from '@nestjs/common';
+import { Model } from 'mongoose';
+
+import { PaymentStrategyFactory } from './strategies/factory/payment-strategy.factory';
+import { PaymentSystemEnum } from './enum/payment-system.enum';
+import { Tariff } from 'src/tariff/schemas/tariff.schema';
+import { TariffService } from 'src/tariff/tariff.service';
+import { UserService } from 'src/user/user.service';
+
 @Injectable()
 export class BillingService {
-  constructor(@InjectModel(Payment.name) private paymentModel: Model<PaymentDocument>) {}
+  constructor(
+    private readonly userService: UserService,
+    private readonly tariffService: TariffService,
+    private readonly paymentStrategyFactory: PaymentStrategyFactory,
+    @InjectModel(Payment.name) private paymentModel: Model<PaymentDocument>,
+  ) {}
 
   async createPayment(
     userId: number,
     chatId: number,
     tariffId: string,
-    tariffPrice: number,
-    paymentAmount: number,
-    createPaymentResponse: any,
+    paymentSystem: PaymentSystemEnum,
     paymentMonths: number,
   ): Promise<Payment> {
-    const payment = Payment.createPayment(
-      userId,
-      chatId,
-      tariffId,
-      tariffPrice,
-      paymentAmount,
-      createPaymentResponse,
-      paymentMonths,
-    );
-    return this.paymentModel.create(payment);
+    const user = await this.userService.findOneByUserId(userId);
+
+    if (!user) throw new Error('User not found');
+
+    const tariff: Tariff = await this.tariffService.getOneById(tariffId);
+
+    if (!tariff) throw new Error('Tariff not found');
+
+    const paymentStrategy = this.paymentStrategyFactory.createPaymentStrategy(paymentSystem);
+
+    const payment = await paymentStrategy.createPayment(userId, chatId, tariffId, paymentMonths);
+
+    return payment;
   }
 
   async getPendingPayments(): Promise<Payment[]> {
     return this.paymentModel.find({ status: 'pending' }).exec();
   }
 
-  async updatePaymentStatus(id: string, status: string): Promise<void> {
-    await this.paymentModel.updateOne({ _id: id }, { status: status }).exec();
+  async updatePaymentStatus(id: string, status: string, isFinal: boolean): Promise<void> {
+    await this.paymentModel.updateOne({ _id: id }, { status, isFinal }).exec();
   }
 
   async findPaymentById(id: string): Promise<Payment> {
     return this.paymentModel.findById(id).exec();
+  }
+
+  async validatePayment(paymentId: string): Promise<boolean> {
+    const payment = await this.paymentModel.findById(paymentId).exec();
+    if (!payment) throw new Error(`Payment with id ${paymentId} not found`);
+
+    const paymentStrategy = this.paymentStrategyFactory.createPaymentStrategy(payment.paymentSystem);
+
+    return paymentStrategy.validateTransaction(paymentId);
   }
 }
