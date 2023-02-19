39939e7bf17426ac77cca7607d40137aaf8371e9
Evgenii Fedoryachenko <mdwit0r@gmail.com>
2023-02-19 12:41:27 +0100

    chore(bot): update payments scene

--- src/scenes/payment.scene.ts
+++ src/scenes/payment.scene.ts
@@ -1,4 +1,4 @@
-import { Action, Ctx, Scene } from 'nestjs-telegraf';
+import { Action, Ctx, Scene, SceneEnter } from 'nestjs-telegraf';
 
 import { AbstractScene } from '../abstract/abstract.scene';
 import { CommandEnum } from '../enum/command.enum';
@@ -6,6 +6,8 @@ import { PaymentService } from '../payment/payment.service';
 import { PaymentSystemEnum } from 'src/payment/enum/payment-system.enum';
 import { Context } from 'src/interfaces/context.interface';
 import { Markup } from 'telegraf';
+import { replyOrEdit } from 'src/utils/reply-or-edit.util';
+import { SCENES } from 'src/constants/scenes.const';
 
 @Scene(CommandEnum.PAYMENT)
 export class PaymentScene extends AbstractScene {
@@ -13,11 +15,20 @@ export class PaymentScene extends AbstractScene {
     super();
   }
 
+  @SceneEnter()
+  async onSceneEnter(@Ctx() ctx: Context) {
+    this.logger.log(ctx.scene.session.current);
+    const scene = SCENES[ctx.scene.session.current];
+
+    await replyOrEdit(ctx, scene.text, Markup.inlineKeyboard(scene.buttons));
+  }
+
   @Action(CommandEnum.PAY_WITH_CRYPTOMUS)
   async payWithCriptomus(@Ctx() ctx: Context) {
     const { paymentMonths, tariffId } = ctx.session;
 
     const paymentSystem = PaymentSystemEnum.CYPTOMUS;
+
     const payment = await this.paymentService.createPayment(
       ctx.from.id,
       ctx.chat.id,
@@ -25,7 +36,8 @@ export class PaymentScene extends AbstractScene {
       paymentSystem,
       paymentMonths,
     );
-    await ctx.reply(
+    await replyOrEdit(
+      ctx,
       `Чтобы оплатить подписку для выбранного вами тарифа, вам нужно перейти к оплате, нажав на кнопку ниже.\n\nПосле того как вы оплатите, я автоматически вам поменяю тариф.`,
       Markup.inlineKeyboard([[Markup.button.url('👉 перейти к оплате', payment.url)]]),
     );
