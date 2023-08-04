56d4c0740156080d9695f6ff172742620bdb7e74
Evgenii Fedoryachenko <mdwit0r@gmail.com>
2023-08-04 14:19:11 +0200

    feat(yookassa-client.service.ts): add YookassaClientService class with generatePayment method
    The YookassaClientService class is responsible for interacting with the YooKassa API to generate payments. It has a constructor that initializes the necessary configuration values from the ConfigService. The generatePayment method takes in the amount, paymentId, comment, and receipt as parameters and returns a Promise of type Payment. It creates a new instance of YooCheckout and uses it to create a payment with the provided payload.

--- libs/yookassa-client/src/yookassa-client.service.ts
+++ libs/yookassa-client/src/yookassa-client.service.ts
@@ -1,4 +1,41 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { ICreatePayment, YooCheckout } from '@a2seven/yoo-checkout';
+import { IReceipt } from '@a2seven/yoo-checkout/build/types/IReceipt';
+import { Payment } from '@a2seven/yoo-checkout/build/models';
 
 @Injectable()
-export class YookassaClientService {}
+export class YookassaClientService {
+  private readonly token: string;
+  private readonly shopId: string;
+  private readonly successURL: string;
+
+  private readonly yooKassa: YooCheckout;
+  constructor(private readonly configService: ConfigService) {
+    this.token = this.configService.get('YOOKASSA_SECRET');
+    this.shopId = this.configService.get('YOOKASSA_SHOP_ID');
+    this.successURL = this.configService.get('DOMAIN') + '/payment/yookassa/success';
+
+    this.yooKassa = new YooCheckout({ shopId: this.shopId, secretKey: this.token });
+  }
+
+  async generatePayment(amount: number, paymentId: string, comment: string, receipt: IReceipt): Promise<Payment> {
+    const checkout = new YooCheckout({ shopId: this.shopId, secretKey: this.token });
+
+    const createPayload: ICreatePayment = {
+      amount: {
+        value: amount.toString(),
+        currency: 'RUB',
+      },
+      capture: true,
+      confirmation: {
+        type: 'redirect',
+        return_url: this.successURL,
+      },
+      receipt,
+      description: comment,
+    };
+
+    return await checkout.createPayment(createPayload, paymentId);
+  }
+}
